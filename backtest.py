8498017313cb4d29cce0c24a4e8134d01440757d
leofracca <leonardofraccaroli1999@gmail.com>
2022-03-06 13:56:50 +0100

    first commit

---
+++ backtest.py
@@ -0,0 +1,27 @@
+import backtrader as bt
+import datetime  # For datetime objects
+from strategies import TestStrategy
+
+if __name__ == '__main__':
+    cerebro = bt.Cerebro()
+    cerebro.broker.setcash(100000.0)
+
+    # Create a Data Feed
+    data = bt.feeds.YahooFinanceCSVData(
+        dataname='oracle.csv',
+        # Do not pass values before this date
+        fromdate=datetime.datetime(2000, 1, 1),
+        # Do not pass values after this date
+        todate=datetime.datetime(2000, 12, 31),
+        reverse=False)
+
+    cerebro.adddata(data)
+    cerebro.addstrategy(TestStrategy)
+
+    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    cerebro.run()
+
+    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    cerebro.plot()
