98aab9e8f304bb83f2a7cf5ebb4041318bec73e3
leofracca <leonardofraccaroli1999@gmail.com>
2022-03-13 19:16:59 +0100

    bug fixes backtest.py and updated the dataset

--- backtest.py
+++ backtest.py
@@ -1,6 +1,5 @@
 import backtrader as bt
 import datetime
-from Strategies.testStrategy import TestStrategy
 from Strategies.AIStrategy import AIStrategy
 
 if __name__ == '__main__':
@@ -10,11 +9,19 @@ if __name__ == '__main__':
     # Create a Data Feed
     data = bt.feeds.GenericCSVData(
         dataname='datas/15min_BTC-USDT.csv',
-        fromdate=datetime.datetime(2021, 1, 1),
-        todate=datetime.datetime(2021, 1, 7),
-        dtformat="%Y-%m-%d %H:%M:%S",
         timeframe=bt.TimeFrame.Minutes,
-        compression=1)
+        compression=15,
+        fromdate=datetime.datetime(2022, 1, 1),
+        todate=datetime.datetime(2022, 1, 7),
+        dtformat='%Y-%m-%d %H:%M:%S',
+        datetime=0,
+        open=1,
+        high=2,
+        low=3,
+        close=4,
+        volume=5,
+        openinterest=-1,
+    )
     data.addfilter(bt.filters.SessionFilter(data))
 
     cerebro.adddata(data)
