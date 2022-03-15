06b89e7d751cfa8c2ce10f82ee86d26f46615524
leofracca <leonardofraccaroli1999@gmail.com>
2022-03-15 19:16:53 +0100

    Added reward calculation

--- Strategies/AIStrategy.py
+++ Strategies/AIStrategy.py
@@ -20,9 +20,6 @@ class AIStrategy(bt.Strategy):
         # 200 EMA
         self.ema200 = bt.indicators.ExponentialMovingAverage(period=200)
 
-        # RSI
-        self.rsi = bt.indicators.RSI_SMA()
-
         # To keep track of when the crossover crosses up or down
         # (True if it crosses up, False otherwise)
         self.macd_cross_up = False
@@ -31,6 +28,8 @@ class AIStrategy(bt.Strategy):
         self.dataclose = self.datas[0].close
         # To keep track of pending orders
         self.order = None
+
+        self.reward = 1
 
     def notify_order(self, order):
         if order.status in [order.Submitted, order.Accepted]:
@@ -42,8 +41,12 @@ class AIStrategy(bt.Strategy):
         if order.status in [order.Completed]:
             if order.isbuy():
                 self.log('BUY EXECUTED, %.2f' % order.executed.price)
+                self.buy_price = order.executed.price
             elif order.issell():
                 self.log('SELL EXECUTED, %.2f' % order.executed.price)
+                sell_price = order.executed.price
+
+                self.reward += (sell_price - self.buy_price) * 0.0001
 
             self.bar_executed = len(self)
 
@@ -60,7 +63,8 @@ class AIStrategy(bt.Strategy):
         # Check if we have any open position
         if not self.position:
             # If not, check if the current candle closes above the 200 EMA
-            if self.dataclose[0] > self.ema200[0]:
+            if self.dataclose[0] >= self.ema200[0]:
+                # Open a long position
 
                 # Check if the MACD lines crosses up or down
                 if self.crossover[0] > 0.0:
@@ -70,8 +74,13 @@ class AIStrategy(bt.Strategy):
 
                 # If the MACD crosses up and the current candle closes above the parabolic SAR
                 if self.macd_cross_up and self.psar[0] < self.dataclose[0]:
+
+                    # How many bitcoin should we buy?
+                    amount = self.reward
+                    print(amount)
+
                     # Buy
-                    self.buy()
+                    self.buy(size=amount)
                     self.log('BUY CREATE, %.2f' % self.dataclose[0])
 
                     # Reset this variable for the next trade
@@ -82,9 +91,8 @@ class AIStrategy(bt.Strategy):
                     self.stop_loss = self.psar[0]
                     print('TAKE PROFIT = ' + str(self.take_profit))
                     print('STOP_LOSS = ' + str(self.stop_loss))
-                    print(self.psar[0])
-                    print(self.dataclose[0])
 
                     # Set the OCO sell order
-                    oco_profit = self.sell(exectype=bt.Order.Limit, price=self.take_profit)
-                    oco_loss = self.sell(exectype=bt.Order.Stop, price=self.stop_loss, oco=oco_profit)
+                    oco_profit = self.sell(exectype=bt.Order.Limit, price=self.take_profit, size=amount)
+                    oco_loss = self.sell(exectype=bt.Order.Stop, price=self.stop_loss, size=amount, oco=oco_profit)
+                    
