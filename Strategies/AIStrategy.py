509ba020520dfe9a41d7aaa1c3de2f2ed7d2cce8
leofracca <leonardofraccaroli1999@gmail.com>
2022-03-16 17:25:28 +0100

    Added short positions

--- Strategies/AIStrategy.py
+++ Strategies/AIStrategy.py
@@ -30,6 +30,7 @@ class AIStrategy(bt.Strategy):
         self.order = None
 
         self.reward = 1
+        self.is_short_position = False
 
     def notify_order(self, order):
         if order.status in [order.Submitted, order.Accepted]:
@@ -46,7 +47,7 @@ class AIStrategy(bt.Strategy):
                 self.log('SELL EXECUTED, %.2f' % order.executed.price)
                 sell_price = order.executed.price
 
-                self.reward += (sell_price - self.buy_price) * 0.0001
+                #self.reward += (sell_price - self.buy_price) * 0.0001
 
             self.bar_executed = len(self)
 
@@ -62,18 +63,20 @@ class AIStrategy(bt.Strategy):
 
         # Check if we have any open position
         if not self.position:
+
             # If not, check if the current candle closes above the 200 EMA
             if self.dataclose[0] >= self.ema200[0]:
-                # Open a long position
+                self.is_short_position = False
 
                 # Check if the MACD lines crosses up or down
                 if self.crossover[0] > 0.0:
                     self.macd_cross_up = True
-                elif self.crossover[0] < 0.0:
+                else:  # self.crossover[0] <= 0.0
                     self.macd_cross_up = False
 
                 # If the MACD crosses up and the current candle closes above the parabolic SAR
                 if self.macd_cross_up and self.psar[0] < self.dataclose[0]:
+                    # Open a long position
 
                     # How many bitcoin should we buy?
                     amount = self.reward
@@ -83,16 +86,56 @@ class AIStrategy(bt.Strategy):
                     self.buy(size=amount)
                     self.log('BUY CREATE, %.2f' % self.dataclose[0])
 
-                    # Reset this variable for the next trade
-                    self.macd_cross_up = False
-
                     # Calculate take profit and stop loss (for sell order)
                     self.take_profit = self.dataclose[0] * 2 - self.psar[0]
                     self.stop_loss = self.psar[0]
                     print('TAKE PROFIT = ' + str(self.take_profit))
                     print('STOP_LOSS = ' + str(self.stop_loss))
 
-                    # Set the OCO sell order
-                    oco_profit = self.sell(exectype=bt.Order.Limit, price=self.take_profit, size=amount)
-                    oco_loss = self.sell(exectype=bt.Order.Stop, price=self.stop_loss, size=amount, oco=oco_profit)
-                    
+            else:
+                self.is_short_position = True
+
+                # Check if the MACD lines crosses up or down
+                if self.crossover[0] >= 0.0:
+                    self.macd_cross_up = True
+                else:  # self.crossover[0] < 0.0
+                    self.macd_cross_up = False
+
+                # If the MACD crosses down and the current candle closes below the parabolic SAR
+                if not self.macd_cross_up and self.psar[0] > self.dataclose[0]:
+                    # Open a short position
+
+                    # How many bitcoin should we short?
+                    amount = self.reward
+                    #print(amount)
+
+                    # Open the short
+                    self.sell(size=amount)
+                    self.log('SHORT CREATE, %.2f' % self.dataclose[0])
+
+                    # Calculate take profit and stop loss (for sell order)
+                    self.take_profit = self.dataclose[0] * 2 - self.psar[0]
+                    self.stop_loss = self.psar[0]
+                    print('TAKE PROFIT = ' + str(self.take_profit))
+                    print('STOP_LOSS = ' + str(self.stop_loss))
+        else:
+            # We have an open position (long or short)
+            # So check if the current candle hits the take profit or the stop loss and sell
+            if not self.is_short_position:
+                if self.data.high[0] >= self.take_profit:
+                    # Long position PROFIT
+                    self.log('Closing long position in profit, %.2f' % self.take_profit)
+                    self.close()
+                elif self.data.low[0] <= self.stop_loss:
+                    # Long position LOSS
+                    self.log('Closing long position in loss, %.2f' % self.stop_loss)
+                    self.close()
+            else:
+                if self.data.high[0] >= self.stop_loss:
+                    # Short position LOSS
+                    self.log('Closing short position in loss, %.2f' % self.stop_loss)
+                    self.close()
+                elif self.data.low[0] <= self.take_profit:
+                    # Short position PROFIT
+                    self.log('Closing short position in profit, %.2f' % self.take_profit)
+                    self.close()
